57f126fbe5cb001fb4ca662e70e203318d2d83bf
Leonardo Sonsin <leonardo.sonsin@blink.com.br>
2022-10-19 10:40:25 -0300

    Configurações para ler e carregar os arquivos json e imprimir o caminho deles no DataUploaderManifest

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,14 +6,17 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
+import 'package:flutter_study_app/data_uploader_screen.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_study_app/main.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(GetMaterialApp(
+      home: DataUploaderScreen(),
+    ));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
